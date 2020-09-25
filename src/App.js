5920ff05e34ba2117bdfa9a9f312310fa27cd9d7
Nikal Morgan <nikalmorgan11@gmail.com>
2020-09-25 18:03:41 -0500

    api fetch correctly working must try to fetch foregin keys

--- src/App.js
+++ src/App.js
@@ -2,35 +2,49 @@ import React from 'react';
 
 
 class App extends React.Component{
+
   constructor(props){
     super(props);
     this.state = {
-      shoes : []
+      shoes : [{size: 7.5, brand_name: 'Nik', manufacturer: 'Nike', color: 'Orange', 
+                material: 'cotton', shoe_type: 'sneaker', fasten_type: 'Shoe strings'}]
     }
   }
 
   componentDidMount(){
-    fetch("http://127.0.0.1:8000/shoes/")
-    .then(res => res.json)
-    .then(data => this.setState({shoes : data.results}))
+    fetch("http://127.0.0.1:8000/api/shoes")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      this.setState({shoes : data})
+    })
   }
+
+
   render(){
   return (
     <div>
       <h1>Slik's Shoe Store</h1>
-      <ul>
-      {this.state.shoes.map(shoe =>
-        <li>
-          <li>{shoe.size}</li>
-          <li>{shoe.brand_name}</li>
-          <li>{shoe.manufacturer}</li>
-          <li>{shoe.color}</li>
-          <li>{shoe.material}</li>
-          <li>{shoe.shoe_type}</li>
-          <li>{shoe.fasten_type}</li>
-        </li> 
+      {this.state.shoes.map((shoe, id) =>
+            <div key={id}>
+            <ul>
+              <li>Size: {shoe.size}</li>
+              <li>Brand Name: {shoe.brand_name}</li>
+              <li>
+                Manufacturer: <a href={shoe.manufacturer}>{shoe.manufacturer}</a>
+              </li>
+              <li>
+                Color: <a href={shoe.color}>{shoe.color}</a>
+              </li>
+              <li>Material: {shoe.material}</li>
+              <li>
+                Type: <a href={shoe.shoe_type}>{shoe.shoe_type}</a>
+              </li>
+              <li>Fasten Type: {shoe.fasten_type}</li>
+              <br />
+            </ul>
+          </div> 
       )}
-      </ul>
     </div>
   );
   }
