48d0841204e26b3448ad7db650aea85f96dc3821
Nikal Morgan <nikalmorgan11@gmail.com>
2020-09-25 19:50:27 -0500

    Got two of the three foregin keys to show

--- src/App.js
+++ src/App.js
@@ -6,17 +6,41 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      shoes : [{size: 7.5, brand_name: 'Nik', manufacturer: 'Nike', color: 'Orange', 
-                material: 'cotton', shoe_type: 'sneaker', fasten_type: 'Shoe strings'}]
+      shoes : []
     }
   }
 
+  fetchedFKS(data){
+    this.setState({shoes : data})
+  }
   componentDidMount(){
     fetch("http://127.0.0.1:8000/api/shoes")
     .then((res) => res.json())
-    .then((data) => {
+    .then(async (data) => {
       console.log(data)
-      this.setState({shoes : data})
+      let new_data = []
+      let shoe = {size: 0, brand_name: '', manufacturer: '', color: '', 
+      material: '', shoe_type: '', fasten_type: ''}
+      for(let i = 0; i < data.length; i++){
+        shoe.size = data[i].size
+        shoe.brand_name = data[i].brand_name
+        shoe.manufacturer =await fetch(`${data[i].manufacturer}`)
+        .then((res) => res.json())
+        .then((data2) => data2)
+        shoe.color = await fetch(`${data[i].color}`)
+          .then((res) => res.json())
+          .then((data2) => data2)
+        shoe.shoe_type = await fetch(`${data[i].shoe_type}`)
+          .then((res) => res.json())
+          .then((data2) => data2)
+        shoe.material = data[i].material
+        shoe.shoe_type = data[i].shoe_type
+        shoe.fasten_type = data[i].fasten_type
+        new_data.push(shoe)
+        console.log(shoe)
+        // console.log(new_data)
+      }
+      this.fetchedFKS(new_data)
     })
   }
 
@@ -31,14 +55,19 @@ class App extends React.Component{
               <li>Size: {shoe.size}</li>
               <li>Brand Name: {shoe.brand_name}</li>
               <li>
-                Manufacturer: <a href={shoe.manufacturer}>{shoe.manufacturer}</a>
+                Manufacturer: <a href={shoe.manufacturer.website}>{shoe.manufacturer.name}</a>
               </li>
               <li>
-                Color: <a href={shoe.color}>{shoe.color}</a>
+                Color: {shoe.color.color_name}
               </li>
               <li>Material: {shoe.material}</li>
               <li>
-                Type: <a href={shoe.shoe_type}>{shoe.shoe_type}</a>
+                Type: 
+                  {
+                    fetch(`${shoe.shoe_type}`)
+                    .then((res) => res.json())
+                    .then((data2) => data2)
+                  }
               </li>
               <li>Fasten Type: {shoe.fasten_type}</li>
               <br />
